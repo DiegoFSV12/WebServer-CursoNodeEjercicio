3622c23bc3145f9bf18378cf9b98a3ff98d0169e
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-01-25 16:07:53 -0500

    Endpoint PUT para actualizar listas de ToDo

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,9 +1,9 @@
 import { Request,Response } from "express";
 
 const todos = [
-    {id:1,text:'Buy milk',createdAt: new Date()},
-    {id:2,text:'Buy meat',createdAt: null},
-    {id:3,text:'Buy bread',createdAt: new Date()},
+    {id:1,text:'Buy milk',completedAt: new Date()},
+    {id:2,text:'Buy meat',completedAt: null},
+    {id:3,text:'Buy bread',completedAt: new Date()},
 ];
 
 export class TodosController{
@@ -15,7 +15,10 @@ export class TodosController{
     /*Traer ToDo segun su Id*/
     public getTodoById = (req:Request,res:Response)=>{
         const id = +req.params.id;/*Transforma el string a un numero*/
-        if(isNaN(id)) res.status(404).json({error:`ID argument is not a number`});
+        if(isNaN(id)) {
+            res.status(404).json({error:`ID argument is not a number`});
+            return;
+        }
         const todo = todos.find(todo=>todo.id === id);
         if(todo){
             res.json(todo);
@@ -30,9 +33,39 @@ export class TodosController{
         const newToDo = {
             id: todos.length+1,
             text: text,
-            createdAt: null
+            completedAt: null
         };
         todos.push(newToDo);
         res.json(newToDo);
+    }
+
+    //Usamos PUT para actualizar un ToDo
+    public updateToDo = (req:Request, res:Response) => {
+        //Buscar y validar
+        const id = +req.params.id;
+        if(isNaN(id)) res.status(404).json({error:`ID argument is not a number`});
+        const todo = todos.find(todo=>todo.id === id);
+        if(!todo) {
+            res.status(404).json({error:`ToDo with id ${id} not found`});
+            return;
+        }
+        const {text, completedAt} = req.body;
+        if(!text) res.status(404).json({error:`Text property is required`});
+
+        todo.text = text || todo.text;
+        if(completedAt === null){
+            todo.completedAt = null;
+        }else{
+            todo.completedAt = new Date(completedAt || todo.completedAt);
+        }
+
+
+        todos.forEach((todo, index)=>{
+            if(todo.id === id){
+                todos[index] = todo
+            }
+        })
+
+        res.json(todo);
     }
 }
