25c3de640c68b90870f89ac8af559056f9077284
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-01-27 16:31:06 -0500

    Controlador actualizado

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -30,7 +30,7 @@ export class TodosController{
 
     public createToDo = async(req:Request,res:Response)=>{
         const [error,createToDoDTO] = CreateToDoDTO.create(req.body);
-        if(error) return res.status(404).json({error});
+        if(error) res.status(404).json({error});
         const todo = await prisma.todo.create({
             data: createToDoDTO!
         });
@@ -41,15 +41,15 @@ export class TodosController{
     public updateToDo = async(req:Request, res:Response) => {
         //Buscar y validar
         const id = +req.params.id;
-        if(isNaN(id)) return res.status(404).json({error:`ID argument is not a number`});
+        if(isNaN(id)) res.status(404).json({error:`ID argument is not a number`});
         const todo = await prisma.todo.findUnique({
             where: {
                 id : id
             }
         });
-        if(!todo) return res.status(404).json({error: `Todo with id ${id} not found`});
+        if(!todo) res.status(404).json({error: `Todo with id ${id} not found`});
         const {text, completedAt} = req.body;
-        if(!text) return res.status(404).json({error:`Text property is required`});
+        if(!text) res.status(404).json({error:`Text property is required`});
         const updatetodo = await prisma.todo.update({
             where: {
                 id : id
@@ -71,7 +71,7 @@ export class TodosController{
                 id : id
             }
         });
-        if(!todo) return res.status(404).json({error: `Todo with id ${id} not found`});
+        if(!todo) res.status(404).json({error: `Todo with id ${id} not found`});
         const deleted = await prisma.todo.delete({
             where: {
                 id : id
