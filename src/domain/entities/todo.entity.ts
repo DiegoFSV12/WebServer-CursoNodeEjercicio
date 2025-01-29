4369bb7dc4db5d2ae54425a077021e35b7dd11f6
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-01-29 15:43:30 -0500

    Entity

--- src/domain/entities/todo.entity.ts
+++ src/domain/entities/todo.entity.ts
@@ -9,5 +9,17 @@ export class TodoEntity {
         return !!this.completedAt;//Retorna true si existe y false sino
     }
     
-    
+    public static fromObject(object:{[key:string]:any}){
+        const {id, text, completedAt} = object;
+        if(!id) throw "Id is required";
+        if(!text) throw "Text is required";
+        let newCompletedAt;
+        if(completedAt){
+            newCompletedAt = new Date(completedAt);
+            if(isNaN(newCompletedAt.getTime())){
+                throw "CompletedAt is not a valid date";
+            }
+        }
+        return new TodoEntity(id,text,completedAt);
+    }
 }
