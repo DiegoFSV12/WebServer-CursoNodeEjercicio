c0f86a88ee9dc7a98762bd04fbb859c03e33a4c2
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-01-29 16:43:22 -0500

    Rutas actualizadas

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,12 +1,14 @@
-import { Router } from "express";
-import { TodosController } from "./todos/controller";
-import { TodoRoutes } from "./todos/routes";
+import { Router } from 'express';
+import { TodoRoutes } from './todos/routes';
 
-export class AppRoutes{
-    static get routes(): Router{
+
+export class AppRoutes {
+
+    static get routes(): Router {
         const router = Router();
-        const todoController = new TodosController();
         router.use('/api/todos', TodoRoutes.routes);
         return router;
     }
+
+
 }
--- src/presentation/todos/routes.ts
+++ src/presentation/todos/routes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { TodosController } from "./controller";
+import { TodoDatasourceImpl } from "../../infrastructure/datasource/todo.datasource";
+import { TodoRepositoryImpl } from "../../infrastructure/repositories/todo.repository";
 
 
 export class TodoRoutes{
     static get routes(): Router{
         const router = Router();
-        const todoController = new TodosController();
+        const datasource = new TodoDatasourceImpl();
+        const todoRepository = new TodoRepositoryImpl(datasource);
+        const todoController = new TodosController(todoRepository);
         router.get('/', todoController.getTodos);
         router.get('/:id', todoController.getTodoById);
         router.post('/', todoController.createToDo);
