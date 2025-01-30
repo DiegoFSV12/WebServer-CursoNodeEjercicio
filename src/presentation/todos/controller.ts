89b18ec551ef5b25692fa27e2f9de34b05625ea7
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-01-30 17:22:25 -0500

    Controlador actualizado para casos de uso

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,7 +1,7 @@
 import { Request,Response } from "express";
 import { prisma } from "../../data/postgresql";
 import { CreateToDoDTO, UpdateToDoDTO } from "../../domain/dtos";
-import { TodoRepository } from "../../domain";
+import { CreateTodo, DeleteTodo, GetAllTodo, GetTodo, TodoRepository, UpdateTodo } from "../../domain";
 
 export class TodosController{
     constructor(
@@ -9,25 +9,27 @@ export class TodosController{
     ){}
     /*Traer todos los ToDo */
     public getTodos = async(req:Request,res:Response)=>{
-        const todos = await this.todoRepository.getAll();
-        res.json(todos);
+        new GetAllTodo(this.todoRepository)
+        .execute()
+        .then(todo=>res.json(todo))
+        .catch(error => res.status(400).json({error}))
     }
     /*Traer ToDo segun su Id*/
     public getTodoById = async (req:Request,res:Response)=>{
         const id = +req.params.id;/*Transforma el string a un numero*/
-        try {
-            const todo = await this.todoRepository.findById(id);
-            res.json(todo);
-        } catch (error) {
-            res.status(404).json({error});
-        }
+        new GetTodo(this.todoRepository)
+        .execute(id)
+        .then(todo=>res.json(todo))
+        .catch(error => res.status(400).json({error}))
     }
 
     public createToDo = async(req:Request,res:Response)=>{
         const [error,createToDoDTO] = CreateToDoDTO.create(req.body);
         if(error) res.status(404).json({error});
-        const todo = await this.todoRepository.create(createToDoDTO!);
-        res.json(todo);
+        new CreateTodo(this.todoRepository)
+        .execute(createToDoDTO!)
+        .then(todo=>res.json(todo))
+        .catch(error => res.status(400).json({error}))
     }
 
     //Usamos PUT para actualizar un ToDo
@@ -36,15 +38,19 @@ export class TodosController{
         const id = +req.params.id;
         const [error, updateTodoDTO] = UpdateToDoDTO.update({...req.body, id});
         if(error) res.status(404).json({error});
-        const todo = await this.todoRepository.updateById(updateTodoDTO!);
-        res.json(todo);
+        new UpdateTodo(this.todoRepository)
+        .execute(updateTodoDTO!)
+        .then(todo=>res.json(todo))
+        .catch(error => res.status(400).json({error}))
     }
 
     //Usamos DELETE para borrar fisicamente un elemento
     public deleteToDo = async(req:Request,res:Response) =>{
         //Buscar y validar
         const id = +req.params.id;
-        const todo = await this.todoRepository.deleteById(id);
-        res.json(todo);
+        new DeleteTodo(this.todoRepository)
+        .execute(id)
+        .then(todo=>res.json(todo))
+        .catch(error => res.status(400).json({error}))
     }
 }
